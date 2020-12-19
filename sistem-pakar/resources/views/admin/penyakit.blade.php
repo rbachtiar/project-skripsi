d314c14e6ca9f14d45365a6de760d763b7f09b74
farras <fadhilfarras008@gmail.com>
2020-12-19 11:15:19 +0700

    aokeee

--- sistem-pakar/resources/views/admin/penyakit.blade.php
+++ sistem-pakar/resources/views/admin/penyakit.blade.php
@@ -5,7 +5,7 @@
             <div class="card">
               <div class="card-header">
                 <h4 class="card-title"> Penyakit</h4>
-                <button class="btn btn-success float-right" data-toggle="modal" data-target="#tambahKelas">Tambah</button>
+                <button class="btn btn-success float-right" data-toggle="modal" data-target="#tambahPenyakit">Tambah</button>
               </div>
                 <div class="card-body">
                     <div class="table-responsive">
@@ -18,7 +18,7 @@
       </div>
 
     <!-- tambah Modal -->
-    <div class="modal fade" id="tambahKelas" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal fade" id="tambahPenyakit" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
             <div class="modal-header">
@@ -28,14 +28,26 @@
                 </button>
             </div>
             <div class="modal-body">
-            <form">
+            <form id="form-penyakit" method="">
                 <div class="form-group">
-                    <label for="exampleInputEmail1">Nama penyakit</label>
-                    <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="Masukkan Nama Kelas" id="nama" required>
+                    <label for="exampleInputEmail1">Kode Penyakit</label>
+                    <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="Masukkan Kode Gejala" name="kode_penyakit" required>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Nama Penyakit</label>
+                    <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="Masukkan Gejala" name="penyakit" required>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Info Penyakit</label>
+                    <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="Masukkan Gejala" name="info" required>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Solusi Penyakit</label>
+                    <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="Masukkan Gejala" name="solusi" required>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <input type="submit" class="btn btn-primary" value="Submit" id="tambah-kelas">
+                    <input type="submit" class="btn btn-primary" value="Submit" id="submit-gejala">
                 </div>
             </form>
             </div>
@@ -44,25 +56,36 @@
     </div>
 
     <!-- edit modal -->
-    <div class="modal fade" id="editKelas" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal fade" id="editPenyakit" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                <h5 class="modal-title" id="exampleModalLongTitle">Edit Gejala</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-            <form method="POST" action="{{url('kelas')}}">
-                @csrf
+            <form id="form-penyakit">
+            <div class="form-group">
+                    <label for="exampleInputEmail1">Kode Penyakit</label>
+                    <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="Masukkan Kode Gejala" name="kode_penyakit_edit" required>
+                </div>
                 <div class="form-group">
-                    <label for="exampleInputEmail1">Nama Kelas</label>
-                    <input type="text" class="form-control" id="nama-edit" aria-describedby="emailHelp" placeholder="Masukkan Nama Kelas" required>
+                    <label for="exampleInputEmail1">Nama Penyakit</label>
+                    <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="Masukkan Gejala" name="penyakit_edit" required>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Info Penyakit</label>
+                    <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="Masukkan Gejala" name="info_edit" required>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Solusi Penyakit</label>
+                    <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="Masukkan Gejala" name="solusi_edit" required>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <input type="submit" class="btn btn-primary" value="Submit" id="submit">
+                    <input type="submit" class="btn btn-primary" value="Submit" id="edit-penyakit">
                     <input type="hidden" id="id">
                 </div>
             </form>
@@ -101,66 +124,37 @@
             });
         }
 
-        //tambah firebase data sensor
-        $('body').on('click', '#tambah-kelas', function(e) {
+        //tambah gejala
+        $('body').on('submit', '#form-penyakit', function(e) {
             e.preventDefault();
-            var formData = new FormData()
-            var nama = $("#nama").val();
-            // console.log(nama);
-            formData.append('nama', nama);
+            var data = $("#form-penyakit").serialize();
                 $.ajax({
-                    type: 'GET',
-                    url: '/kelas/tambah',
-                    data: {nama:nama}, 
+                    type: 'POST',
+                    url: '{{url('a/penyakit')}}',
+                    data: data, 
                     success: function(data) {
-                        if(data.success == "true") {
+                        if(data.store == "success") {
                             Swal.fire({
-                            icon: 'success',
-                            title: 'Success',
-                            text: 'Berhasil tambah kelas!',
-                            showConfirmButton: false,
-                            timer: 1200
-                        })
-                        $.ajax({
-                            type: 'GET',
-                            url: '/kelas/id',
-                            success: function(data) {
-                                addSensor(data.data)
-                            }
-                        });
-                        setTimeout(reload, 3000);
+                                icon: 'success',
+                                title: "Sukses",
+                                text: 'Berhasil Menambahkan Penyakit',
+                                timer: 1200,
+                                showConfirmButton: false
+                            });
+                            $("#tambahPenyakit").modal("hide");
+                            $("#form-penyakit").trigger("reset");
+                            loadPenyakit();
                         }
                     }
                 });
         });
-
-        function reload() {
-            location.reload();
-        }
-        //fungi firebase
-        function addSensor(id)
-        {
-            var sensorRef = firebase.database();
-            var sensor = sensorRef.ref("kelas/" + id);
-            sensor.set({
-            lampu : 'off'
-            });
-        }
-
-        function deleteSensor(id)
-        {
-            var sensorRef = firebase.database();
-            var sensor = sensorRef.ref("kelas/" + id);
-            sensor.remove();
-
-        }
-
-        $('body').on('click', '.btn-delete-kelas', function(e) {
+        //delete
+        $('body').on('click', '.btn-delete-penyakit', function(e) {
             e.preventDefault();
             var id = $(this).data('id');
             // console.log(id)
             Swal.fire({
-                title: 'Anda yakin ingin menghapus kelas?'+
+                title: 'Anda yakin ingin menghapus Penyakit?'+
                 'Anda tidak dapat membatalkan aksi ini!',
                 showDenyButton: true,
                 showCancelButton: true,
@@ -171,60 +165,56 @@
                 if (!result.isConfirmed) {
                     $.ajax({
                         type: 'GET',
-                        url: 'kelas/delete/' + id,
+                        url: "{{url('a/penyakit/delete')}}" + '/' + id,
                         contentType: false,
                         processData: false,
                         success: function(data) {
                             if(data.delete == 'success') {
                                 Swal.fire('Deleted!', '', 'success')
-                                deleteSensor(id)
-                                setTimeout(reload, 3000);
+                                loadPenyakit();
                             }
                         }
                     });
                 } else if (result.isDenied) {
-                    Swal.fire('kelas tidak dihapus', '', 'info')
+                    Swal.fire('Penyakit tidak dihapus', '', 'info')
                 }
             })
         });
 
-        //edit kelas
-        $('body').on('click', '.btn-edit-kelas', function(e) {
+        //edit penyakit
+        $('body').on('click', '.btn-edit-penyakit', function(e) {
             e.preventDefault();
-            $('#editKelas').modal('show');
+            $('#editPenyakit').modal('show');
             var id = $(this).data('id');
             $("#id").val(id);
-            console.log(id);
+            // console.log(id);
             $.ajax({
                 type: 'GET',
-                url: '/kelas/edit/' + id,
+                url: '{{url('a/penyakit/edit')}}' + '/' +id,
                 contentType: false,
                 processData: false,
                 success: function(data) {
-                    if(data.success == true) {
-                        $('#editKelas').modal('show');
-                        console.log(data.data[0].room_name)
-                        $("#nama-edit").val(data.data[0].room_name);
-                    }
+                        $('#editPenyakit').modal('show');
+                        $("input[name=kode_penyakit_edit]").val(data.data[0].kode_penyakit);
                 }
             });
         });
 
         //update 
-        $('body').on('click', '#submit', function(e) {
+        $('body').on('submit', '#submit', function(e) {
             e.preventDefault();
             var id = $("#id").val();
             var nama = $("#nama-edit").val();
             $.ajax({
                 type: 'GET',
-                url: '/kelas/update/' + id,
+                url: '/penyakit/update/' + id,
                 data: {nama:nama},
                 success: function(data) {
                     if(data.update == 'success') {
                         Swal.fire({
                             icon: 'success',
                             title: 'Success',
-                            text: 'Berhasil update kelas!',
+                            text: 'Berhasil update Penyakit!',
                             showConfirmButton: false,
                             timer: 1200
                         })
